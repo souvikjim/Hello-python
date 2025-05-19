0d3681cfb8ba588f08086a54b7c2f35eed410ab0
Souvik Mondal <souvik.mondal@thermofisher.com>
2025-05-19 21:54:05 +0530

    started learning

---
+++ app.py
@@ -0,0 +1,9 @@
+x = 1
+course = ' python programming '
+print(course[:])
+print(course.upper())
+print(course.lower())
+print(course.strip())
+print(course.lstrip())
+print(course.rstrip())
+print(course.find('pro'))
