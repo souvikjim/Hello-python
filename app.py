8ef3228c31cb2e3febffc34904d37ffebe66eeb8
Souvik Mondal <souvik.mondal@thermofisher.com>
2025-05-19 22:04:44 +0530

    string and numbers

--- app.py
+++
@@ -1,9 +0,0 @@
-x = 1
-course = ' python programming '
-print(course[:])
-print(course.upper())
-print(course.lower())
-print(course.strip())
-print(course.lstrip())
-print(course.rstrip())
-print(course.find('pro'))
---
+++ app1.py
@@ -0,0 +1,26 @@
+import math
+# string
+course = ' python programming '
+print(course[:])
+print(course.upper())
+print(course.lower())
+print(course.strip())
+print(course.lstrip())
+print(course.rstrip())
+print(course.find('pro'))
+print(course.replace('p', 'g'))
+print('pro' in course)
+print('bal' not in course)
+
+
+# number
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+print(round(2.6))
+print(abs(-2.9))
+print(math.ceil(2.7))
