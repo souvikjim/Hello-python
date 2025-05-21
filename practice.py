98308948d5d0ade5767ef2755abafd2d25a86ec9
Souvik Mondal <souvik.mondal@thermofisher.com>
2025-05-21 11:52:08 +0530

    practiced

---
+++ practice.py
@@ -0,0 +1,14 @@
+# num1 = int(input('put number 1:'))
+# num2 = int(input('put number 2:'))
+# sum = 0
+# if (num1 and num2):
+#     sum = num1+num2
+# print(sum)
+
+const = [9, 92, 3, 4, 5, 6]
+leyt = const
+sorted = sorted(const)
+leyt[0] = 99
+print(const.sort())
+print(leyt)
+print(sorted)
